2b6de1667e9194ccdea529ead6dba72dce0dcaae
Jeonghan Kim <91442091+watermelonswag@users.noreply.github.com>
2022-04-10 23:53:08 +0900

    my first website~!

---
+++ Kimjeonghan-js.js
@@ -0,0 +1,143 @@
+$(function () {
+    $(".goEducation").on('click',function() {
+        $(".Myself-mainname").show();
+        $(".Myself-subname").show();
+        $(".Myself-mainname2").hide();
+        $(".Myself-subname2").hide();
+        $(".Myself").hide();
+        $(".Contact").hide();
+        $(".Experience").hide();
+        $(".Hobbies").hide();
+        $(".Education").show();
+        $(".arrowEducation").hide();
+        $(".dotEducation").show();
+        $(".arrowContact").show();
+        $(".dotContact").hide();
+        $(".arrowExperience").show();
+        $(".dotExperience").hide();
+        $(".arrowHobbies").show();
+        $(".dotHobbies").hide();
+        $(".arrowMyself").show();
+        $(".dotMyself").hide();
+    })
+})
+
+$(function () {
+    $(".goMyself").on('click',function() {
+        $(".Myself-mainname").show();
+        $(".Myself-subname").show();
+        $(".Myself-mainname2").hide();
+        $(".Myself-subname2").hide();
+        $(".Myself").show();
+        $(".Education").hide();
+        $(".Contact").hide();
+        $(".Experience").hide();
+        $(".Hobbies").hide();
+        $(".arrowMyself").hide();
+        $(".dotMyself").show();
+        $(".arrowContact").show();
+        $(".dotContact").hide();
+        $(".arrowEducation").show();
+        $(".dotEducation").hide();
+        $(".arrowExperience").show();
+        $(".dotExperience").hide();
+        $(".arrowHobbies").show();
+        $(".dotHobbies").hide();
+    })
+})
+
+$(function () {
+    $(".goContact").on('click',function() {
+        $(".Myself-mainname").show();
+        $(".Myself-subname").show();
+        $(".Myself-mainname2").hide();
+        $(".Myself-subname2").hide();
+        $(".Myself").hide();
+        $(".Education").hide();
+        $(".Contact").show();
+        $(".Experience").hide();
+        $(".Hobbies").hide();
+        $(".arrowContact").hide();
+        $(".dotContact").show();
+        $(".arrowEducation").show();
+        $(".dotEducation").hide();
+        $(".arrowExperience").show();
+        $(".dotExperience").hide();
+        $(".arrowHobbies").show();
+        $(".dotHobbies").hide();
+        $(".arrowMyself").show();
+        $(".dotMyself").hide();
+    })
+})
+
+$(function () {
+    $(".goExperience").on('click',function() {
+        $(".Myself-mainname").show();
+        $(".Myself-subname").show();
+        $(".Myself-mainname2").hide();
+        $(".Myself-subname2").hide();
+        $(".Myself").hide();
+        $(".Education").hide();
+        $(".Contact").hide();
+        $(".Experience").show();
+        $(".Hobbies").hide();
+        $(".arrowExperience").hide();
+        $(".dotExperience").show();
+        $(".arrowContact").show();
+        $(".dotContact").hide();
+        $(".arrowEducation").show();
+        $(".dotEducation").hide();
+        $(".arrowHobbies").show();
+        $(".dotHobbies").hide();
+        $(".arrowMyself").show();
+        $(".dotMyself").hide();
+    })
+})
+
+$(function () {
+    $(".goHobbies").on('click',function() {
+        $(".Myself-mainname").show();
+        $(".Myself-subname").show();
+        $(".Myself-mainname2").hide();
+        $(".Myself-subname2").hide();
+        $(".Myself").hide();
+        $(".Education").hide();
+        $(".Contact").hide();
+        $(".Experience").hide();
+        $(".Hobbies").show();
+        $(".arrowHobbies").hide();
+        $(".dotHobbies").show();
+        $(".arrowContact").show();
+        $(".dotContact").hide();
+        $(".arrowEducation").show();
+        $(".dotEducation").hide();
+        $(".arrowExperience").show();
+        $(".dotExperience").hide();
+        $(".arrowMyself").show();
+        $(".dotMyself").hide();
+    })
+})
+
+$(function () {
+    $(".goMain").on('click',function() {
+        $(".Myself-mainname").hide();
+        $(".Myself-subname").hide();
+        $(".Myself-mainname2").show();
+        $(".Myself-subname2").show();
+        $(".Myself").hide();
+        $(".Education").hide();
+        $(".Contact").hide();
+        $(".Experience").hide();
+        $(".Hobbies").hide();
+        $(".arrowHobbies").show();
+        $(".dotHobbies").hide();
+        $(".arrowContact").show();
+        $(".dotContact").hide();
+        $(".arrowEducation").show();
+        $(".dotEducation").hide();
+        $(".arrowExperience").show();
+        $(".dotExperience").hide();
+        $(".arrowMyself").show();
+        $(".dotMyself").hide();
+    })
+})
